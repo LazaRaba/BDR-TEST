ae0ea248d1a6fd4cefa9eb6fc08474d7c15468c3
laza <lazadevop@gmail.com>
2023-04-06 19:06:40 +0200

    account creation page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,11 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-//--------------------Deplacement de la route account--------------------------------------------
-#[Route('/account', name: 'app_account')]
+//----------------------------------------------------------------
 class AccountController extends AbstractController
 {
-    #[Route('/', name: 'account')]
+    #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
         return $this->render('account/index.html.twig', [
