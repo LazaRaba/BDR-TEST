998abdcf5495fbd942c8f7d81f603900722841a2
laza <lazadevop@gmail.com>
2023-04-06 15:21:18 +0200

    modif du css signup

---
+++ templates/partials/login.php
@@ -0,0 +1,28 @@
+
+<!-- Css signup -->
+<link rel="stylesheet" href="assets/signup.css">
+
+<div class="grand-parent container">
+    <form class="form-parent" action="" method="POST">
+        <div class="form-container">
+            <div class="form" id="sign-in-form">
+                <h1 class="signTitle">Connexion</h1>
+                <div class="fields">
+                    <label for="email">email</label>
+                    <input type="text" name="email"  placeholder="Votre  email" autocomplete="off">
+                    <input type="password" name="password" placeholder="Mot de passe" autocomplete="off">
+                    
+                </div>
+                <div class="submit-container">
+                    <button type="submit" name="validate" class="btn">se connecter</button>
+                    <p class="link"><a href="signup.php">Vous n'avez pas de compte ? Inscrivez-vous</a></p>
+                    <p class="link"><a href="reset_password.php">Mot de passe oublié</a></p>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+    
+
