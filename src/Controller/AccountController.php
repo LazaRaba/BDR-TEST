443c0887f51e7e205b9b220efe1ddd082d115334
laza <lazadevop@gmail.com>
2023-04-06 18:50:31 +0200

    creation page account user

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+//--------------------Deplacement de la route account--------------------------------------------
+#[Route('/account', name: 'app_account')]
+class AccountController extends AbstractController
+{
+    #[Route('/', name: 'account')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
